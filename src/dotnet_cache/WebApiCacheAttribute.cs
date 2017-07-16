43942a26b25e34e6a6c7996865dfe9b952812277
asabla <sparven89@gmail.com>
2017-07-16 12:35:34 +0200

    Initial commit for dotnet_cache

---
+++ src/dotnet_cache/WebApiCacheAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace dotnet_cache
+{
+    public class WebApiCacheAttribute : ActionFilterAttribute
+    {
+
+    }
+}
